53fec6812e7a735035e7ca5bd3f595a9aedd28d8
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-11-03 16:48:14 -0300

    fix: changing the html language to en-US following the parttern

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="en-US">
       <body>{children}</body>
     </html>
   );
